5042a0869e95a04e69e726ab31d3ffee1b448f1e
windowsagent <juanjphc@gmail.com>
2024-10-14 15:08:15 -0600

    bug: Add inline policy for allowing to upload to s3

--- oidc-github.tf
+++ oidc-github.tf
@@ -5,4 +5,22 @@ module "oidc-github" {
   github_repositories = [
     "windowsagent/MEAN-Stack-User-Registration-Front-End"
   ]
+
+  iam_role_inline_policies = {
+    "s3-bucket-site-policy": data.aws_iam_policy_document.s3_bucket_site_policy.json
+  }
+}
+
+
+data "aws_iam_policy_document" "s3_bucket_site_policy" {
+  statement {
+    actions   = [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject",
+        "s3:ListBucket"
+    ]
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.www_bucket.arn}/*"]
+  }
 }
