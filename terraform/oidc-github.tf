90294c3cfede2858c2499d6927166fd19774a34b
windowsagent <juanjphc@gmail.com>
2024-10-15 18:02:38 -0600

    attach ecr public policy

--- terraform/oidc-github.tf
+++ terraform/oidc-github.tf
@@ -16,13 +16,10 @@ module "oidc-github" {
 data "aws_iam_policy_document" "ecr_container_policy" {
   statement {
     actions   = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload"
+      "ecr:*",
+      "ecr-public:*",
+      "cloudtrail:LookupEvents",
+      "iam:CreateServiceLinkedRole"
     ]
     effect    = "Allow"
     resources = ["*"]
